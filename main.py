84d99e64e4d30337572721b06fcb7ffb1dfb2a96
KatieT6 <katarzynatysz66@gmail.com>
2023-03-14 11:52:54 +0100

    Zmiany wielomianu

--- main.py
+++ main.py
@@ -10,10 +10,32 @@ print("3. Wykladnicza")
 
 
 def przedzial_poszukiwania():
-    print("Okresl przedział: \n Od:")
+    print("Okresl przedział: ")
+    x1 = float(input("Wprowadź początek przedziału: "))
+    x2 = float(input("Wprowadź koniec przedziału: "))
+    return x1, x2
 
 
-user_input = int(input("Wybierz jedna z funkcji:"))
+def wielomian_wspolczynniki():
+    stopien = int(input("Wprowadz stopien wielomianu"))
+    n = []
+    for i in range(0, stopien + 1):
+        n.append(input("Podaj współczynnik a" + str(i) + ": "))
+        print("to jest a dla x" + str(i) + ": " + str(n))
+    return n
+
+
+def rysuj_wielomian(x1, x2, n, stopien):
+    x = np.linspace(x1, x2)
+    fx = []
+    for i in range(len(x)):
+        fx.append(n[stopien]*x[i]**stopien)
+
+
+# def horner():
+
+
+user_input = int(input("Wybierz jedna z funkcji: "))
 
 match user_input:
     case 1:
@@ -22,6 +44,8 @@ match user_input:
 
     case 2:
         print("Trygonometryczna")
+        x1, x2 = przedzial_poszukiwania()
 
     case 3:
         print("Wykladnicza")
+        x1, x2 = przedzial_poszukiwania()
