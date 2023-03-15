f046e5dde8a34a734c627b2bc4e3a4613f581eac
Oliwia Karbowa 242416 <242416@edu.p.lodz.pl>
2023-03-16 00:18:22 +0100

    wykresy

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from email.policy import default
 import matplotlib as plt
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.polynomial import Polynomial
 import horner
 import bisekcja
 
@@ -22,20 +23,35 @@ def rage():
     return x1, x2
 
 
-def polynomial_coefficient():
-    stopien = int(input("Wprowadz stopien wielomianu"))
-    n = []
-    for i in range(0, stopien + 1):
-        n.append(input("Podaj współczynnik a" + str(i) + ": "))
-        print("to jest a dla x" + str(i) + ": " + str(n))
-    return n, stopien
+def polynomial_coefficients():
+    stopien = int(input("Wprowadz stopien wielomianu: "))
+    a = []
+    for i in reversed(range(0, stopien + 1)):
+        a.append(input("Podaj współczynnik a" + str(i) + ": "))
+        print("to jest a dla x" + str(i) + ": " + str(a))
+    return a, stopien
 
 
-def print_polynomial(x1, x2, n, stopien):
-    x = np.linspace(x1, x2)
-    fx = []
-    for i in range(len(x)):
-        fx.append(n[stopien] * x[i] ** stopien)
+
+def print_poly(coeff):
+    p = np.poly1d([1, 2, 3])
+    print(p)
+    x = np.arange(-5, 5, 0.01)
+    fx = wartosc_func([1, 2, 3], x)
+
+    plt.xlabel("os X")
+    plt.ylabel("os Y")
+
+    # plt.plot(x, [fx[i] for i in x])
+
+    plt.xticks([i for i in range(-20, 20, 5)])
+    plt.yticks([i for i in range(-20, 20, 5)])
+    plt.grid()
+    plt.xlim(-10, 10)
+    plt.ylim(-5, 20)
+    plt.plot(x, fx)
+    plt.show()
+
 
 
 user_input = int(input("Wybierz jedna z funkcji: "))
