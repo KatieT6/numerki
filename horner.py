1309aab25dd59cca9ea1566b45097cb6e3694bb7
Oliwia Karbowa 242416 <242416@edu.p.lodz.pl>
2023-03-16 00:22:28 +0100

    wykresy v2

--- horner.py
+++ horner.py
@@ -1,11 +1,11 @@
-def horner(wsp, st, x):
+def horner_scheme(wsp, st, x):
     wynik = wsp[0]
 
-    for i in range(1, st + 1):
+    for i in range(1, int(st) + 1):
         wynik = wynik * x + wsp[i]
 
     return wynik
 
-
+#wsp od najwyższego do najniższego
 if __name__ == "__main__":
-    print(horner([2, 2, 10, 5], 3, 5))
+    print(horner_scheme([1, 2, 3], 2, 5))
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from email.policy import default
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import matplotlib as plt
+
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.polynomial import Polynomial
 import horner
 import bisekcja
-
+#
 print("1. Wielomianowa")
 print("2. Trygonometryczna")
 print("3. Wykladnicza")
@@ -31,6 +31,13 @@ def polynomial_coefficients():
         print("to jest a dla x" + str(i) + ": " + str(a))
     return a, stopien
 
+def wartosc_func(coeff, x):
+    f = []
+    for i in x:
+        value = horner.horner_scheme([1, 2, 3], 2, x)
+        f.append(value)
+
+    return f
 
 
 def print_poly(coeff):
@@ -42,7 +49,7 @@ def print_poly(coeff):
     plt.xlabel("os X")
     plt.ylabel("os Y")
 
-    # plt.plot(x, [fx[i] for i in x])
+     plt.plot(x, [fx[i] for i in x])
 
     plt.xticks([i for i in range(-20, 20, 5)])
     plt.yticks([i for i in range(-20, 20, 5)])
@@ -59,9 +66,10 @@ user_input = int(input("Wybierz jedna z funkcji: "))
 match user_input:
     case 1:
         print("Wielomianowa")
-        x1, x2 = rage()
-        coefficient = polynomial_coefficient()
-        bisekcja.bisect_method_accuracy()
+        #x1, x2 = rage()
+        #coefficient = polynomial_coefficients()[0]
+        print_poly([1, 2, 3])
+        #bisekcja.bisect_method_accuracy()
 
     case 2:
         print("Trygonometryczna")
