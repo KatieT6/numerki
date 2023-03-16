59e9ea523b792e8991d4cccae883b57c4320a663
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2023-03-16 17:55:58 +0100

    plots good

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from numpy.polynomial import Polynomial
 import horner
 import bisekcja
+
 #
 print("1. Wielomianowa")
 print("2. Trygonometryczna")
@@ -27,38 +28,89 @@ def polynomial_coefficients():
     stopien = int(input("Wprowadz stopien wielomianu: "))
     a = []
     for i in reversed(range(0, stopien + 1)):
-        a.append(input("Podaj współczynnik a" + str(i) + ": "))
+        a.append(int(input("Podaj współczynnik a" + str(i) + ": ")))
         print("to jest a dla x" + str(i) + ": " + str(a))
     return a, stopien
 
-def wartosc_func(coeff, x):
+
+def value_of_poly(coeff, x, stopien):
     f = []
     for i in x:
-        value = horner.horner_scheme([1, 2, 3], 2, x)
+        value = horner.horner_scheme(coeff, stopien, i)
         f.append(value)
-
     return f
 
 
-def print_poly(coeff):
-    p = np.poly1d([1, 2, 3])
-    print(p)
-    x = np.arange(-5, 5, 0.01)
-    fx = wartosc_func([1, 2, 3], x)
+def print_poly(coeff, stopien):
+    # p = np.poly1d(coeff)
+    # print(p)
+    x = np.arange(-20, 20, 0.01)
+    fx = value_of_poly(coeff, x, stopien)
 
-    plt.xlabel("os X")
-    plt.ylabel("os Y")
-
-     plt.plot(x, [fx[i] for i in x])
+    plt.xlabel("oś X")
+    plt.ylabel("oś Y")
 
     plt.xticks([i for i in range(-20, 20, 5)])
     plt.yticks([i for i in range(-20, 20, 5)])
     plt.grid()
-    plt.xlim(-10, 10)
-    plt.ylim(-5, 20)
+    plt.xlim(-20, 20)
+    plt.ylim(-10, 20)
     plt.plot(x, fx)
     plt.show()
 
+
+def value_of_func(choice, x):
+    f = []
+    # 1. y = -3*sin(2*x-1)
+    if choice == 1:
+        for i in x:
+            value = (-3) * np.sin(2 * i - 1)
+            f.append(value)
+        return f
+
+    # 2. y = cos(2*x-sin(-x))
+    elif choice == 2:
+        for i in x:
+            value = np.cos(2 * i - np.sin(-i))
+            f.append(value)
+        return f
+
+    # 3. y = 5^x-3
+    elif choice == 3:
+        for i in x:
+            value = (5**i)-3
+            f.append(value)
+        return f
+
+    # 4. y = 3^(2*x)-6
+    elif choice == 4:
+        for i in x:
+            value = (3**(2*i))-6
+            f.append(value)
+        return f
+
+
+def print_function(choice):
+    x = np.arange(-20, 20, 0.01)
+    fx = value_of_func(choice, x)
+
+    plt.xlabel("oś X")
+    plt.ylabel("oś Y")
+
+    # plt.xticks([i for i in range(-20, 20, 5)])
+    # plt.yticks([i for i in range(-20, 20, 5)])
+
+    plt.xticks()
+
+    plt.grid()
+
+    plt.autoscale(enable=True)
+
+    # plt.xlim(-20, 20)
+    # plt.ylim(-10, 20)
+    plt.plot(x, fx)
+
+    plt.show()
 
 
 user_input = int(input("Wybierz jedna z funkcji: "))
@@ -66,15 +118,21 @@ user_input = int(input("Wybierz jedna z funkcji: "))
 match user_input:
     case 1:
         print("Wielomianowa")
-        #x1, x2 = rage()
-        #coefficient = polynomial_coefficients()[0]
-        print_poly([1, 2, 3])
-        #bisekcja.bisect_method_accuracy()
+        coefficient, st = polynomial_coefficients()
+        print_poly(coefficient, st)
+        x1, x2 = rage()
+        # bisekcja.bisect_method_accuracy()
 
     case 2:
         print("Trygonometryczna")
-        x1, x2 = rage()
+        type_of_func = int(input("Wybierz rodzaj: \n 1. y = -3*sin(2*x-1) \n 2. y = cos(2*x-sin(-x))"))
+        print_function(type_of_func)
+        #x1, x2 = rage()
 
     case 3:
         print("Wykladnicza")
-        x1, x2 = rage()
+        type_of_func = int(input("Wybierz rodzaj: \n 3. y = 5^x-3 \n 4. y = 3^(2*x)-3"))
+        print_function(type_of_func)
+        #x1, x2 = rage()
+    case _:
+        print("Dokonaj wyboru")
