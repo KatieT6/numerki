bb8ced29f8fb9461f5cc4f1c3f14d8d6f6501511
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2023-03-18 12:23:28 +0100

    warynem na krancach przedziału

--- bisection.py
+++ bisection.py
@@ -1,10 +1,12 @@
 def bisect_method_iteration(func, iteration, a, b):
-    for i in (1, iteration):
+    x0 = 0
+    for i in range(iteration):
         x0 = (a + b) / 2
         if func(x0) * func(b) < 0:
             a = x0
         elif func(x0) * func(a) < 0:
             b = x0
+    return x0
 
 
 def bisect_method_accuracy(func, acc, a, b):
@@ -13,5 +15,7 @@ def bisect_method_accuracy(func, acc, a, b):
         return x0
     if func(x0) * func(b) < 0:
         a = x0
+        bisect_method_accuracy(func, acc, a, b)
     elif func(x0) * func(a) < 0:
         b = x0
+        bisect_method_accuracy(func, acc, a, b)
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.polynomial import Polynomial
 import horner
-import bisekcja
+import bisection
 
 #
 print("1. Wielomianowa")
@@ -17,11 +17,7 @@ print("2. Trygonometryczna")
 print("3. Wykladnicza")
 
 
-def rage():
-    print("Okresl przedział: ")
-    x1 = float(input("Wprowadź początek przedziału: "))
-    x2 = float(input("Wprowadź koniec przedziału: "))
-    return x1, x2
+
 
 
 def polynomial_coefficients():
@@ -59,35 +55,28 @@ def print_poly(coeff, stopien):
     plt.show()
 
 
+# funkcja do obliczania wartości wybranej funkcji dla danego x
+def one_value(choice, x):
+    if choice == 1:
+        return (-3) * np.sin(2 * x - 1)
+
+    if choice == 2:
+        return np.cos(2 * x - np.sin(-x))
+
+    if choice == 3:
+        return (5 ** x) - 3
+
+    if choice == 4:
+        return (3 ** (2 * x)) - 6
+
+
+# funkcja okreslajaca zbir wartosci dla danych x-ów
 def value_of_func(choice, x):
     f = []
-    # 1. y = -3*sin(2*x-1)
-    if choice == 1:
-        for i in x:
-            value = (-3) * np.sin(2 * i - 1)
-            f.append(value)
-        return f
-
-    # 2. y = cos(2*x-sin(-x))
-    elif choice == 2:
-        for i in x:
-            value = np.cos(2 * i - np.sin(-i))
-            f.append(value)
-        return f
-
-    # 3. y = 5^x-3
-    elif choice == 3:
-        for i in x:
-            value = (5**i)-3
-            f.append(value)
-        return f
-
-    # 4. y = 3^(2*x)-6
-    elif choice == 4:
-        for i in x:
-            value = (3**(2*i))-6
-            f.append(value)
-        return f
+    for i in x:
+        value = one_value(choice, i)
+        f.append(value)
+    return f
 
 
 def print_function(choice):
@@ -113,6 +102,42 @@ def print_function(choice):
     plt.show()
 
 
+def zero_point_method():
+    print(" a) spełnienie warunku nałożonego na dokładność \n b) osiągnięcie zadanej liczby iteracji")
+    choose_method = int(input("Wybierz wybiera kryterium zatrzymania algorytmu:"))
+    match choose_method:
+        case "a":
+            epsilon = float(input("Podaj dokladnosc:"))
+            # metoda bisekcji
+            #bisection.bisect_method_accuracy()
+            # metoda siecznych
+        case "b":
+            iterations = int(input("Podaj liczbe iteracji:"))
+            # metoda bisekcji
+
+            # metoda siecznych
+
+
+def rage(func_type, coeff, n):
+    print("Okresl przedział: ")
+    x1 = float(input("Wprowadź początek przedziału: "))
+    x2 = float(input("Wprowadź koniec przedziału: "))
+
+    if func_type == 0:
+        if horner.horner_scheme(coeff, n, x1) * horner.horner_scheme(coeff, n, x2) < 0:
+            return x1, x2
+
+        elif horner.horner_scheme(coeff, n, x1) * horner.horner_scheme(coeff, n, x2) > 0:
+            return rage(func_type, coeff, n)
+
+    elif func_type != 0:
+        if one_value(func_type, x1) * one_value(func_type, x2) < 0:
+            return x1, x2
+
+        elif one_value(func_type, x1) * one_value(func_type, x2) > 0:
+            return rage(func_type, coeff, n)
+
+
 user_input = int(input("Wybierz jedna z funkcji: "))
 
 match user_input:
@@ -120,19 +145,19 @@ match user_input:
         print("Wielomianowa")
         coefficient, st = polynomial_coefficients()
         print_poly(coefficient, st)
-        x1, x2 = rage()
-        # bisekcja.bisect_method_accuracy()
+        x1, x2 = rage(0, coefficient, st)
+        zero_point_method()
 
     case 2:
         print("Trygonometryczna")
         type_of_func = int(input("Wybierz rodzaj: \n 1. y = -3*sin(2*x-1) \n 2. y = cos(2*x-sin(-x))"))
         print_function(type_of_func)
-        #x1, x2 = rage()
+        x1, x2 = rage(type_of_func, 0, 0)
 
     case 3:
         print("Wykladnicza")
         type_of_func = int(input("Wybierz rodzaj: \n 3. y = 5^x-3 \n 4. y = 3^(2*x)-3"))
         print_function(type_of_func)
-        #x1, x2 = rage()
+        x1, x2 = rage(type_of_func, 0, 0)
     case _:
-        print("Dokonaj wyboru")
+        print("Wybierz z pośród podanych opcji!")
