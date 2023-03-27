570155cc67b12a352674bbb55439a8338989697c
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2023-03-28 00:22:50 +0200

    super UX: wczytanie i wybranie macierzy

---
+++ Zad_2/main.py
@@ -0,0 +1,33 @@
+def read_coefficients(filename):
+    # Open the file and read the contents
+    with open(filename, 'r') as f:
+        contents = f.read()
+
+    # Split the contents into sections, each section represents a system of equations
+    sections = contents.split('\n\n')
+
+    # Print the list of available systems and prompt the user to choose one
+    print('Available systems:')
+    for i, section in enumerate(sections):
+        print(f'{i+1}:')
+        print(section)
+        print('')
+    choice = int(input('Choose a system: '))
+
+    # Get the coefficients and constants for the chosen system
+    coeffs_lines = sections[choice-1].split('\n')
+    A = []
+    b = []
+    for line in coeffs_lines:
+        # Split the line into coefficients and constants
+        coeffs = line.split('|')[0].split()
+        const = float(line.split('|')[1])
+
+        # Append the coefficients and constants to A and b
+        A.append([float(c) for c in coeffs])
+        b.append(const)
+
+    return A, b
+
+
+A, b = read_coefficients('data.txt')
